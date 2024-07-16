01b8e70f5f90c1fd06a89c57d13a4b3a35e78ca5
JJprieto23 <109244342+JJprieto23@users.noreply.github.com>
2024-07-16 10:33:13 -0500

    Update MainActivity.kt

--- app/src/main/java/co/edu/sena/myapptwo/MainActivity.kt
+++ app/src/main/java/co/edu/sena/myapptwo/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Joan David Moreno Guzmán")
+                    Greeting("Julian David Prieto Serrato")
                 }
             }
         }
